a6c38ed1b6d53a59846ee9f9e0eb63d52ba15ba0
thebrahminator <vishwa.nath@outlook.com>
2017-08-13 01:00:53 +0530

    Finished Fetching videoID and channelID, stored it in CSV

--- youtubeapi.py
+++ youtubeapi.py
@@ -3,40 +3,54 @@ import json
 import csv
 import pickle
 import random
-
-
-
+from itertools import repeat
+import time
 api_key = "AIzaSyDraPMr8KRfkux5u9DgCjfWh1SA_xJmIl8"
 test_api_key = "AIzaSyD1fGnUYGPjdfpklW9PmjbQkOqlUKxQ3o8"
 
-url = "https://www.googleapis.com/youtube/v3/search?key="+test_api_key+"&type=video&part=snippet&q=bathing" \
-                                                                       "&maxResults=50"
-
-r = requests.get(url=url)
+#r = requests.get(url=url)
 
 #print(r)
-raw_data = r.json()
+#raw_data = r.json()
 
-item_list = raw_data["items"]
-
+#item_list = raw_data["items"]
 idsfile = open('videoandchannelid.csv', "a")
+queriesfile = open('channel_names.csv', 'r')
+queriesfile_csv = csv.reader(queriesfile)
 heading = ("videoId", "channelId")
 video_channel_id = []
-#print(item_list)
+bridge = csv.DictWriter(idsfile, heading)
+bridge.writeheader()
 
 
-for item in item_list:
-    ids = {}
-    #print(item.keys())
-    #print(item["id"]["videoId"], end=" ")
-    #print(item["snippet"]["channelId"])
-    ids["videoId"] = item["id"]["videoId"]
-    ids["channelId"] = item["snippet"]["channelId"]
-    print(ids)
-    video_channel_id.append(ids)
+for data in queriesfile_csv:
+    nextPageToken = ""
+    for i in repeat(None,4):
+        video_channel_id = []
+        url = "https://www.googleapis.com/youtube/v3/search?key=" + test_api_key \
+            + "&type=video&part=snippet" \
+                "&q="+data[0]+"&maxResults=50&pageToken="+nextPageToken
+        r = requests.get(url=url)
+        raw_data = r.json()
+        item_list = raw_data["items"]
+        for item in item_list:
+            ids = {}
+            #print(item.keys())
+            # print(item["id"]["videoId"], end=" ")
+            # print(item["snippet"]["channelId"])
+            if "videoId" in item["id"].keys() and "channelId" in item["snippet"].keys():
+                ids["videoId"] = item["id"]["videoId"]
+                ids["channelId"] = item["snippet"]["channelId"]
+                print(ids)
+                video_channel_id.append(ids)
+        for row in video_channel_id:
+            bridge.writerow(row)
+        nextPageToken = raw_data["nextPageToken"]
+    time.sleep(5)
 
 
-print(video_channel_id)
+
+#print(video_channel_id)
 bridge = csv.DictWriter(idsfile, heading)
 for row in video_channel_id:
     bridge.writerow(row)
