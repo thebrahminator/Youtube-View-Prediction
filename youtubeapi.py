2d810044c0ed5c2e36cf43a826ac64597e3edc79
thebrahminator <vishwa.nath@outlook.com>
2017-08-12 22:42:14 +0530

    Figured out how to search videos

---
+++ youtubeapi.py
@@ -0,0 +1,13 @@
+import requests
+import json
+import csv
+import pickle
+import random
+
+
+
+api_key = "AIzaSyDraPMr8KRfkux5u9DgCjfWh1SA_xJmIl8"
+
+url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyDraPMr8KRfkux5u9DgCjfWh1SA_xJmIl8&type=video&part=snippet&q=boating%7Csailing&resultsPerPage=50"
+
+categories = ["comedy", "jump%20cuts", "madras%20central", "smile%20settai", ]
